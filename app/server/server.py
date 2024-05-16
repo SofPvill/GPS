f089038e03736ef449bf53c18543871e89de0d40
gerardmtz <gerartmartinez@gmail.com>
2024-05-15 19:27:50 -0600

    Update & Checkpoint: minimal functional server Created a minimal functional server running in Python; the structure of the project has changed

---
+++ app/server/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+with open('./model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    time_value = data['Time']
+
+    X_new = np.array([[time_value]])
+    y_pred = model.predict(X_new)
+    
+    print("Unwrapped prediction: ", y_pred)
+
+    return jsonify(prediction=y_pred.tolist())
+
+if __name__ == '__main__':
+    app.run(port=3000)
--- model-ml/model.py
+++ model-ml/model.py
@@ -11,24 +11,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import warnings
 import json
-import os
 
 # model importation
 import pickle
-import sys
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# reading the Dataset
-file_path = sys.argv[1]
-demand_df = pd.read_csv(file_path)
+demand_df = pd.read_csv("../assets/dataset/base-historical-product-demand.csv")
 
 demand_df = demand_df[demand_df['Date'].notnull()]
-
-min_date = demand_df['Date'].min()
-max_date = demand_df['Date'].max()
-
-print(demand_df['Order_Demand'].dtypes)
 
 demand_df['Date'] = pd.to_datetime(demand_df['Date'])
 demand_df['Order_Demand'] = pd.to_numeric(demand_df['Order_Demand'], errors='coerce')
@@ -36,83 +27,7 @@ demand_df['Order_Demand'] = pd.to_numeric(demand_df['Order_Demand'], errors='coe
 demand_df['Date'] = pd.to_datetime(demand_df['Date'])
 demand_df_group = demand_df.groupby(pd.Grouper(key='Date', freq='M'))['Order_Demand'].sum().reset_index()
 
-# Create the images directory
-os.makedirs('./../assets/images/', exist_ok=True)
-
-sns.set_style("whitegrid")
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=demand_df_group, x='Date', y='Order_Demand', color = '#18B15A')
-plt.title('Demanda de pedidos a lo largo del tiempo')
-plt.xlabel('Fecha')
-plt.ylabel('Total de pedidos')
-plt.savefig('./../assets/images/demanda-tiempo-general.png')
-
 demand_df = demand_df[demand_df['Date']>'2011-12-31']
-
-demand_df_group_warehouse = demand_df.groupby(['Date', 'Warehouse'])['Order_Demand'].sum().reset_index()
-
-g = sns.relplot(data=demand_df_group_warehouse, 
-                x='Date', 
-                y='Order_Demand', 
-                kind="line",
-                hue='Warehouse',
-                palette=["#EF9A9A", "#673AB7", "#039BE5", "#FB8C00"],
-                col='Warehouse',
-                col_wrap=2,  
-                height=3, aspect=1.5)
-plt.savefig('../assets/images/demanda-grupo-almacen.png')
-
-whse_s_df = demand_df[demand_df['Warehouse']=='Whse_S']
-whse_a_df = demand_df[demand_df['Warehouse']=='Whse_A']
-whse_c_df = demand_df[demand_df['Warehouse']=='Whse_C']
-whse_j_df = demand_df[demand_df['Warehouse']=='Whse_J']
-
-whse_s_df_group_category = whse_s_df.groupby(['Date', 'Product_Category'])['Order_Demand'].sum().reset_index().sort_values('Order_Demand', ascending=False)
-plt.figure(figsize=(7, 5))
-sns.barplot(data=whse_s_df_group_category, x='Product_Category', y='Order_Demand', color ="#EF9A9A")
-plt.title('Demanda de producto alamacen S')
-plt.xlabel('Categoria de producto')
-plt.xticks(rotation=90)
-plt.ylabel('Demanda del producto')
-plt.savefig('../assets/images/demanda-producto-als.png')
-
-whse_a_df_group_category = whse_a_df.groupby(['Date', 'Product_Category'])['Order_Demand'].sum().reset_index().sort_values('Order_Demand', ascending=False)
-plt.figure(figsize=(7, 5))
-sns.barplot(data=whse_a_df_group_category, x='Product_Category', y='Order_Demand', color ="#673AB7")
-plt.title('Demanda de producto alamacen A')
-plt.xlabel('Categoria de producto')
-plt.xticks(rotation=90)
-plt.ylabel('Demanda del producto')
-plt.savefig('../assets/images/demanda-producto-ala.png')
-
-whse_c_df_group_category = whse_c_df.groupby(['Date', 'Product_Category'])['Order_Demand'].sum().reset_index().sort_values('Order_Demand', ascending=False)
-plt.figure(figsize=(7, 5))
-sns.barplot(data=whse_c_df_group_category, x='Product_Category', y='Order_Demand', color ="#039BE5")
-plt.title('Demanda de producto alamacen C')
-plt.xlabel('Categoria de producto')
-plt.xticks(rotation=90)
-plt.ylabel('Demanda del producto')
-plt.savefig('../assets/images/demanda-producto-alc.png')
-
-whse_j_df_group_category = whse_j_df.groupby(['Date', 'Product_Category'])['Order_Demand'].sum().reset_index().sort_values('Order_Demand', ascending=False)
-plt.figure(figsize=(7, 5))
-sns.barplot(data=whse_j_df_group_category, x='Product_Category', y='Order_Demand', color ="#FB8C00")
-plt.title('Demanda de producto alamacen j')
-plt.xlabel('Categoria de producto')
-plt.xticks(rotation=90)
-plt.ylabel('Demanda del producto')
-plt.savefig('../assets/images/demanda-producto-alj.png')
-
-whse_j_df_cat_19 = whse_j_df[whse_j_df['Product_Category']=='Category_019']
-whse_j_df_cat_19_group = whse_j_df_cat_19.groupby(pd.Grouper(key='Date', freq='M'))['Order_Demand'].sum().reset_index()
-
-sns.set_style("whitegrid")
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=whse_j_df_cat_19_group, x='Date', y='Order_Demand', color = '#FB8C00')
-plt.title('Demanda de pedidos de categoria 19 en Almacen J al paso del tiempo')
-plt.xlabel('Fecha')
-plt.ylabel('Total de pedidos')
-plt.savefig('../assets/images/demanda-producto-cat19.png')
 
 category_19_df = demand_df[(demand_df['Warehouse'] == 'Whse_J') & (demand_df['Product_Category'] == 'Category_019')]
 
